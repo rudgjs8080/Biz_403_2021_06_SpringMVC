8d79dff20e5b563da28087706686791289e00153
rudgjs8080 <kkh_8080@naver.com>
2021-06-03 16:08:09 +0900

    0603com

--- SpMVC_010_Hello/src/main/java/com/callor/hello/service/HomeService.java
+++ SpMVC_010_Hello/src/main/java/com/callor/hello/service/HomeService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 /*
  * Spring 프로젝트에서 클래스를 선언하고
  * 각 클래스에 stereotype을 부착하기
- * stererotype :  Component, Controller, Service, Repository ...
+ * stereotype :  Component, Controller, Service, Repository ...
  * 
  * 클래스에 stereotype을 부착하는 순간
  * 	spring에게 이 클래스는 컨테이너에 보관해 달라 라고 요청하는 것
